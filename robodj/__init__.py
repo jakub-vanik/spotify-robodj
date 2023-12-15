498b1b45ccff70443048e8073a4d6a7d8276e54a
Jakub Vanik <jakub.vanik@gmail.com>
2023-12-15 20:45:08 +0100

    Modified application to work with new Babel

--- robodj/__init__.py
+++ robodj/__init__.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
 
 import flask
-import os
+import flask_babel
+import werkzeug.middleware.proxy_fix
 
-from . import babel
 from . import main
 from . import setup
 
-class ReverseProxied:
-
-  def __init__(self, app, script_name):
-    self.app = app
-    self.script_name = script_name
-
-  def __call__(self, environ, start_response):
-    environ["SCRIPT_NAME"] = self.script_name
-    environ["wsgi.url_scheme"] = environ.get("HTTP_X_FORWARDED_PROTO")
-    return self.app(environ, start_response)
-
-def create_app():
-  app = flask.Flask(__name__)
-  babel.babel.init_app(app)
-  app.register_blueprint(main.bp)
-  app.register_blueprint(setup.bp)
-  app.wsgi_app = ReverseProxied(app.wsgi_app, os.environ["SCRIPT_NAME"])
-  return app
+app = flask.Flask(__name__)
+app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(app.wsgi_app, x_proto=1, x_host=1, x_prefix=1)
+flask_babel.Babel(app, locale_selector=lambda: flask.request.accept_languages.best_match(["en", "cs"]))
+app.register_blueprint(main.bp)
+app.register_blueprint(setup.bp)
